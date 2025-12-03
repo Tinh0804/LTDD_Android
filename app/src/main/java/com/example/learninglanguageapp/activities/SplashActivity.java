12db743e28b416c3ba88acc2b7b4a86db3ec59a5
VyHau <vyhau2005@gmail.com>
2025-12-03 16:55:51 +0700

    Add check to convert activity

--- app/src/main/java/com/example/learninglanguageapp/activities/SplashActivity.java
+++ app/src/main/java/com/example/learninglanguageapp/activities/SplashActivity.java
@@ -4,29 +4,42 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.learninglanguageapp.R;
+import com.example.learninglanguageapp.storage.AppDatabase;
+import com.example.learninglanguageapp.storage.DAOs.UserDAO;
+import com.example.learninglanguageapp.utils.SharedPrefsHelper;
 
 @SuppressLint("CustomSplashScreen")
 public class SplashActivity extends AppCompatActivity {
-
-    private static final int SPLASH_TIME = 4000; // 4 giây
+    private static final int SPLASH_TIME = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+        new Handler(Looper.getMainLooper()).postDelayed(this::checkLoginStatus, SPLASH_TIME);
+    }
 
-        // Chờ 3 giây rồi chuyển qua màn hình chính
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(SplashActivity.this, WelcomeActivity.class);
+    private void checkLoginStatus() {
+        new Thread(() -> {
+            UserDAO userDao = AppDatabase.getInstance(this).userDAO();
+            boolean hasToken = new SharedPrefsHelper(getApplicationContext()).isLoggedIn();
+            boolean hasLocalUser = userDao.hasUser();
+            boolean isLoggedIn = hasLocalUser && hasToken;
+            runOnUiThread(() -> {
+                Intent intent;
+                if (isLoggedIn) {
+                    intent = new Intent(SplashActivity.this, MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                } else
+                    intent = new Intent(SplashActivity.this, WelcomeActivity.class);
                 startActivity(intent);
                 finish();
-            }
-        }, SPLASH_TIME);
+            });
+        }).start();
     }
 }
